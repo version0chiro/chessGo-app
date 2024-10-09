344b0e17951f6e4aed961b7d80f32c008b3b151c
version0chiro <sachinsinghroxx@gmail.com>
2024-10-10 00:25:43 +0530

    added tail wind and login route page to retrive the JWT token

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	let username = '';
+	let password = '';
+
+	async function login() {
+		const response = await fetch('http://localhost:8080/login', {
+			method: 'POST',
+			body: JSON.stringify({ username, password }),
+
+			headers: { 'Content-Type': 'application/json' }
+		});
+		const x = await response.text();
+	}
+</script>
+
+<div class="h-screen w-screen flex flex-col gap-4 justify-center items-center">
+	<input type="text" bind:value={username} placeholder="Username" />
+	<input type="password" bind:value={password} placeholder="Password" />
+	<button class="rounded bg-yellow-100 p-5 border border-black" on:click={login}>Login</button>
+	<button class="rounded bg-gray-600 p-5 border text-white border-black">Register</button>
+</div>
